87e97a485b873d3ffa50c924f68c4eb0cf5c9cc4
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-12-26 15:44:56 +0600

    conditional

---
+++ php/conditional.php
@@ -0,0 +1,26 @@
+<?php
+//if and else
+$a = intval(fgets(STDIN));
+if($a % 2 == 0){
+    echo "even";
+}
+else {
+    echo "odd";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ php/operators.php
@@ -0,0 +1,29 @@
+<?php
+// Arithmetic Operator
+$a = 10;
+$b = 20;
+$c = $a + $b;
+echo "Addition: " . $c;
+echo "<br>";
+
+// Comparison Operator
+$e = 10;
+$f = 20;
+
+if ($e > $f) {
+    echo "$e is big";
+} else {
+    echo "$f is big";
+}
+echo "<br>";
+
+// Logical Operator
+$g = 10;
+$h = 20;
+
+if ($g != $h) {   // logical condition
+    echo "$g and $h are not equal";
+} else {
+    echo "$g and $h are equal";
+}
+?>
